ef3592a3ccfb9ed432020ec8591cc8bc02fc01f0
? <18811711520@163.com>
2019-07-17 18:24:45 +0800

    ori

---
+++ syntensor.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+def SynTensor:
+    def __init__(self, n, mlist, Plist):
+        '''
+        :param self:
+        :param n: shape的个数
+        :param mlist: [m1,...,mn]的list
+        :param Plist: P[i][j]是从j到i的映射
+        '''
+        assert len(mlist) == n,'Length of mlist must equal to n'
+        assert size(Plist) == (n,n), 'Size of Plist must be (n,n)'
+
+        self.n = n
+        self.mList = mlist
+        self.N = sum(mlist)
+
+        # 构造索引序列,即前缀和
+        self.indBegin = mlist
+        s = 0
+        for i in range(n):
+            self.indBegin[i] = mlist[0] if i==0 else self.indBegin[i-1]+mlist[i]
+        self.indEnd = [ self.indBegin[i+1] if i!= n-1 else N for i in range(n)  ]
+
+        # 处理Plist的型状，让他变成mi*mj的
+        self.Plist = np.zeros((n,n),np.ndarray)
+        for i in range(n):
+            for j in range(n):
+                self.Plist[i][j] = np.array( Plist[0:mlist[j],0:mlist[i]] )
+
+
+
+        buildP()
+        buildC()
+        buildR()
+        self.Ax,self.Bx,self.Cx = get_init()
+
+    def buildP(self):
+        self.P = np.zeros([self.N,self.N],np.int)
+        for i in range(n):
+            for j in range(n):
+                self.P[indBegin[j]:indEnd[j],indBegin[i]:indEnd[i]] = self.Plist[i][j]
+
+
+    def buildC(self):
+        self.C = np.zeros([n,n,n],np.ndarray)
+        t = range(n)
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    tmp = np.dot(Plist[j][k],Plist[i][j])
+                    tmp = np.dot(Plist[k][i],tmp)
+                    d = np.diag(tmp)
+                    d = np.diag(d)
+                    self.C[i][j][k] = np.where(d > 0, 1,0)
+
+    def getRijk(self,i,j,k):
+        return np.dot(self.Plist[i][j],self.C[i][j][k])
+
+    def buildR(self):
+        self.R = np.zeros([n,n,n],np.ndarray)
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    self.R[i][j][k] = getRijk(i,j,k)
+
