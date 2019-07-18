52e1608e9911da4b76efcd2fca60a5f6f304f5b2
? <18811711520@163.com>
2019-07-18 12:23:28 +0800

    ori

--- syntensor.py
+++ syntensor.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def SynTensor:
+class SynTensor:
     def __init__(self, n, mlist, Plist):
         '''
         :param self:
@@ -9,7 +9,7 @@ def SynTensor:
         :param Plist: P[i][j]是从j到i的映射
         '''
         assert len(mlist) == n,'Length of mlist must equal to n'
-        assert size(Plist) == (n,n), 'Size of Plist must be (n,n)'
+        assert np.shape(Plist) == (n,n), 'Size of Plist must be (n,n)'
 
         self.n = n
         self.mList = mlist
@@ -28,28 +28,27 @@ def SynTensor:
             for j in range(n):
                 self.Plist[i][j] = np.array( Plist[0:mlist[j],0:mlist[i]] )
 
-
-
-        buildP()
-        buildC()
-        buildR()
-        self.Ax,self.Bx,self.Cx = get_init()
+        self.buildP()
+        self.buildC()
+        self.buildR()
+        self.Ax,self.Bx,self.Cx = self.get_init()
 
     def buildP(self):
         self.P = np.zeros([self.N,self.N],np.int)
+        n = self.n
         for i in range(n):
             for j in range(n):
-                self.P[indBegin[j]:indEnd[j],indBegin[i]:indEnd[i]] = self.Plist[i][j]
+                self.P[self.indBegin[j]:self.indEnd[j],self.indBegin[i]:self.indEnd[i]] = self.Plist[i][j]
 
 
     def buildC(self):
+        n = self.n
         self.C = np.zeros([n,n,n],np.ndarray)
-        t = range(n)
         for i in range(n):
             for j in range(n):
                 for k in range(n):
-                    tmp = np.dot(Plist[j][k],Plist[i][j])
-                    tmp = np.dot(Plist[k][i],tmp)
+                    tmp = np.dot(self.Plist[j][k],self.Plist[i][j])
+                    tmp = np.dot(self.Plist[k][i],tmp)
                     d = np.diag(tmp)
                     d = np.diag(d)
                     self.C[i][j][k] = np.where(d > 0, 1,0)
@@ -58,9 +57,10 @@ def SynTensor:
         return np.dot(self.Plist[i][j],self.C[i][j][k])
 
     def buildR(self):
+        n = self.n
         self.R = np.zeros([n,n,n],np.ndarray)
         for i in range(n):
             for j in range(n):
                 for k in range(n):
-                    self.R[i][j][k] = getRijk(i,j,k)
+                    self.R[i][j][k] = self.getRijk(i,j,k)
 
