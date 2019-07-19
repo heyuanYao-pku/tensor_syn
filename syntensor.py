379d9385f6c2e1df0fd51b38d272a7343316a83e
? <18811711520@163.com>
2019-07-19 15:10:12 +0800

    ori

--- syntensor.py
+++ syntensor.py
@@ -6,10 +6,13 @@ class SynTensor:
         :param self:
         :param n: shape的个数
         :param mlist: [m1,...,mn]的list
-        :param Plist: P[i][j]是从j到i的映射
+        :param Plist: P[i][j]是从j到i的映射，并且需要满足P[i][j]^T = P[j][i]
         '''
         assert len(mlist) == n,'Length of mlist must equal to n'
         assert np.shape(Plist) == (n,n), 'Size of Plist must be (n,n)'
+        for i in range(n):
+            for j in range(i+1,n):
+                assert Plist[i][j] == np.transpose(Plist[j][i]) , 'pij should be the transpose of pji'
 
         self.n = n
         self.mList = mlist
@@ -31,7 +34,6 @@ class SynTensor:
         self.buildP()
         self.buildC()
         self.buildR()
-        self.Ax,self.Bx,self.Cx = self.get_init()
 
     def buildP(self):
         self.P = np.zeros([self.N,self.N],np.int)
@@ -42,8 +44,11 @@ class SynTensor:
 
 
     def buildC(self):
+        '''
+        计算 Cijk =  Pki * Pjk * Pij 并舍弃对角线以外的值
+        '''
         n = self.n
-        self.C = np.zeros([n,n,n],np.ndarray)
+        self.Clist = np.zeros([n,n,n],np.ndarray)
         for i in range(n):
             for j in range(n):
                 for k in range(n):
@@ -51,16 +56,44 @@ class SynTensor:
                     tmp = np.dot(self.Plist[k][i],tmp)
                     d = np.diag(tmp)
                     d = np.diag(d)
-                    self.C[i][j][k] = np.where(d > 0, 1,0)
+                    self.Clist[i][j][k] = np.where(d > 0, 1,0)
 
     def getRijk(self,i,j,k):
+        '''
+        :param i:
+        :param j:
+        :param k:
+        :return: 计算Rijk = P[i][j] * C[i][j][k]
+        '''
         return np.dot(self.Plist[i][j],self.C[i][j][k])
 
     def buildR(self):
+        '''
+        :return:
+        Rlist 是分块的tensor
+        R是整个的tensor
+        '''
         n = self.n
-        self.R = np.zeros([n,n,n],np.ndarray)
+        self.Rlist = np.zeros([n,n,n],np.ndarray)
+        self.R = np.zeros([self.N, self.N, n], np.float)
         for i in range(n):
             for j in range(n):
                 for k in range(n):
-                    self.R[i][j][k] = self.getRijk(i,j,k)
+                    self.Rlist[i][j][k] = self.getRijk(i,j,k)
+                    self.R[self.indBegin[j]:self.indEnd[j], self.indBegin[i]:self.indEnd[i],k] = self.Rlist[i][j][k]
+
+
+    def solution(self):
+
+        m_bar,self.Ax,self.Bx,self.Cx = self.get_init()
+
+    def get_init(self):
+
+        eigvalue,eigvector = np.linalg.eig(self.P)
+        idx = eigvalue.argsort()
+        idx = idx[::-1] # 数组调过来改为降序
+
+        # 获得排序后的特征值和特征向量
+        eigvalue = eigvalue[idx]
+        eigvector = eigvector[:,idx]
 
