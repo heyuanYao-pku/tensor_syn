0474415789de48f9da1ee447fe54db7e5d0479d6
? <18811711520@163.com>
2019-07-23 15:12:55 +0800

    ori

--- test.py
+++ test.py
@@ -3,13 +3,15 @@ import numpy as np
 import json
 import sys
 
-
-fname = 'new_data_10.json'
+image_nums = 156
+fname = 'new_data_start_0_end_156_all_156.json'
 sys.stdin = open(fname,'r')
 data = input()
 data = json.loads(data)
 Plist = np.array(data['P'],np.ndarray)
 
+np.set_printoptions(linewidth = np.nan)
+np.set_printoptions(threshold=np.inf)
 mlist = np.array(data['mlist'])
 #print(mlist)
 #print(Plist[1][0])
@@ -42,11 +44,15 @@ Plist = PPP
 tensor = syntensor.SynTensor(n,mlist,Plist)
 
 print('object building finished')
-k = tensor.rounded_solution()
+#sol = tensor.solution()
+#sol =
+k = tensor.rounded_solution(0.5,2,3 )
 print(np.shape(k))
 
-f = open('D:\\paper\\tensor syn\\res.txt','w')
-sys.stdout = f
-np.set_printoptions(linewidth = np.nan)
-np.set_printoptions(threshold=np.inf)
-print(np.float32(k))
+print(k)
+
+tensor.visualize(image_nums ,'D:\\paper\\tensor syn\\data\\image_list.npy', 'D:\\paper\\tensor syn\\tmp_%d'%image_nums)
+#f = open('D:\\paper\\tensor syn\\res.txt','w')
+#sys.stdout = f
+#print(np.float32(sol))
+#print(np.float32(k))
